2d4f8751880574eca0e6fb11ab257bdce1eb6c72
Nuttapong Prommeedet <nuttapongpt@olive.co.th>
2022-02-22 13:31:04 +0700

    Update main.go

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var (
 var Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to THPD Api!")
+	fmt.Fprintf(w, "Welcome to master THPD Api!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
